97419b5f6f1abb9810ded631da9c12484231cdd6
Fiantso Harena <fiantsorav@gmail.com>
2023-07-19 20:54:47 +0300

    fix: Add the Hero component

---
+++ client/src/pages/list/List.jsx
@@ -0,0 +1,14 @@
+import "./list.css";
+import Navbar from "../../components/navbar/Navbar";
+import Hero from "../../components/hero/Hero";
+
+function List() {
+  return(
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+}
+
+export default List;
