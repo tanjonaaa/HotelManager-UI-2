650cb6610084f0e70d13106cc572f17fc440b7a3
Fiantso Harena <fiantsorav@gmail.com>
2023-07-19 10:22:29 +0300

    fix: Initialize the Header component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
-import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header.jsx";
+import Navbar from "../../components/navbar/Navbar.jsx";
+
 import "./home.css";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }
