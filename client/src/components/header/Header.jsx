6f5f54ab19cfe093738dcb7d9121d1f9462372dc
Fiantso Harena <fiantsorav@gmail.com>
2023-07-19 11:53:33 +0300

    fix: Finished the main header component style

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,9 +1,33 @@
 import "./header.css";
 
-function Header() {
-	return(
-		<div>Hello World</div>
-	); 
+function Header({ type }) {
+  {
+    /* This is the header components of our home page */
+  }
+  return (
+    <div className="header">
+      <div
+        className={
+          type === "list" ? "headerContainer listMode" : "headerContainer"
+        }
+      >
+        {type !== "list" && (
+          <>
+            <h1 className="headerTitle">Bienvenue sur DreamHotel !</h1>
+            <p className="headerDescription">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius
+              distinctio pariatur labore vel corporis laboriosam aut molestiae
+              ab aliquam doloremque corrupti expedita odio culpa obcaecati, esse
+              placeat tempore accusantium. Distinctio!
+            </p>
+            <a href="#" class="headerButton">
+              S'identifier / S'enregistrer
+            </a>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
