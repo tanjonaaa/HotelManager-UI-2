0bbff684f0dce99b151c4978ff93958143230b58
Fiantso Harena <fiantsorav@gmail.com>
2023-07-19 08:38:48 +0300

    fix: Fixing the router of the form

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
