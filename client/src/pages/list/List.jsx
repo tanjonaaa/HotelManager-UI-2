a277ab6acb0e82145f52408d8fa4f9c48045baba
Fiantso Harena <fiantsorav@gmail.com>
2023-07-20 15:47:36 +0300

    refactor: Refactoring all code

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/navbar/Navbar";
 import Hero from "../../components/hero/Hero";
 
 function List() {
-  return(
+  return (
     <div>
       <Navbar />
       <Hero />
