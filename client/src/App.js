09341864dce9e67450380c15350b298c45428e74
Fiantso Harena <fiantsorav@gmail.com>
2023-07-25 21:46:17 +0300

    fix: Add the Hotel route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
 
           {/* This is the route of the hotel list page of the application */}
           <Route path="/hotels" element={<List />} />
+
+          {/* This is the route of the hotel item page of the application */}
+          <Route path="/hotels/:id" element={<Hotel/>}/>
         </Routes>
       </BrowserRouter>
     </div>
