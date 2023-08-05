8bb82c3828cfd313d00854f3a80a166a849cbddc
Fiantso Harena <fiantsorav@gmail.com>
2023-08-05 14:36:26 +0300

    style: Make some changes for the style

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -17,7 +17,7 @@ function Featured() {
               className="featuredImg"
             />
             <div className="featuredTitles">
-              <h1>Antananrivo</h1>
+              <h1>Antananarivo</h1>
               <h2>{data[0]} properties</h2>
             </div>
           </div>
@@ -42,6 +42,41 @@ function Featured() {
             <div className="featuredTitles">
               <h1>Toamasina</h1>
               <h2>{data[2]} properties</h2>
+            </div>
+          </div>
+
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Toliara</h1>
+              <h2>{data[3]} properties</h2>
+            </div>
+          </div>
+
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Antsiranana</h1>
+              <h2>{data[4]} properties</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/max500/689422.webp?k=2595c93e7e067b9ba95f90713f80ba6e5fa88a66e6e55600bd27a5128808fdf2&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Antsirabe</h1>
+              <h2>{data[5]} properties</h2>
             </div>
           </div>
         </>
