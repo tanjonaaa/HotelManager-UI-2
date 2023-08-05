e4ea7b5cfec4f88bf4d27ad48b765d11cf8174f6
Fiantso Harena <fiantsorav@gmail.com>
2023-08-05 11:47:20 +0300

    style: Restyle the navbar section

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,23 +2,33 @@ import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { faHandshake, faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Navbar = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className="navbar">
-      <div className="navContainer">
-        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
-          <span className="logo">DreamHotel</span>
-        </Link>
-        {user ? user.username : (
-          <div className="navItems">
-            <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
-          </div>
-        )}
-      </div>
-    </div>
+    <header>
+      <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+        <span className="logo">
+          {" "}
+          <FontAwesomeIcon icon={faHouse} className="logoIcon" /> DreamHotel
+        </span>
+      </Link>
+      <nav className="navbar">
+          <a href="#about">À propos</a>
+          <a href="#property">Propriétés</a>
+          <a href="#contact">Contact</a>
+      </nav>
+      {user ? (
+        user.username
+      ) : (
+        <div className="navItems">
+          <button className="navButton"><FontAwesomeIcon icon={faHandshake}/></button>
+          <button className="navButton"><FontAwesomeIcon icon={faUser}/></button>
+        </div>
+      )}
+    </header>
   );
 };
 
