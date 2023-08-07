c9d4759d07a43e4a75d24ca6877249a8ae76a31e
tanjonaaa <hei.tanjona@gmail.com>
2023-08-07 10:58:44 +0300

    fix: fixed API call to match with API endpoints

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,28 +1,9 @@
-import useFetch from "../../hooks/useFetch";
 import "./featured.css";
 import Antananarivo from "./Antananarivo.jpg";
 import Mahajanga from "./Mahajanga.webp"
 import Toamasina from "./Toamasina.jpg"
-
-function getCityName(cityCode) {
-  switch (cityCode) {
-    case "1":
-      return "Antananarivo";
-    case "2":
-      return "Mahajanga";
-    case "3":
-      return "Toamasina";
-    case "4":
-      return "Fianarantsoa";
-    case "5":
-      return "Toliara";
-    case "6":
-      return "Antsiranana";
-    // Ajoutez d'autres cas ici si nécessaire pour couvrir d'autres codes de ville
-    default:
-      return "Unknown City";
-  }
-}
+import { useFetch } from "use-http";
+import { useEffect, useState } from "react";
 
 function getImageForCity(cityName) {
   switch (cityName) {
@@ -45,28 +26,34 @@ function getImageForCity(cityName) {
 }
 
 function Featured() {
-  const { data, loading, error } = useFetch(
-    "http://localhost:8000/api/hotels/countByCity?cities=1,2,3,4,5,6"
-  );
+  const [data, setData] = useState([]);
+  const {get, response, error, loading} = useFetch('http://localhost:8000');
+
+  async function getCities (){
+    const cities = await get('/city/count');
+    if(response.ok) setData(cities);
+  }
+
+  useEffect(() => {
+    getCities();
+  }, [])
 
   return (
     <div className="featured" id="property">
-      {loading
-        ? "Loading please wait"
-        : data.map((cityData) => {
-            const cityName = getCityName(cityData.city);
-            const imageUrl = getImageForCity(cityName);
-
+      {loading && "Loading"}
+      {error && "Erreur"}
+      {data.map((city) => {
             return (
-              <div key={cityData.city} className="featuredItem">
-                <img src={imageUrl} alt={cityName} className="featuredImg" />
+              <div key={city.id} className="featuredItem">
+                <img src={Antananarivo} alt={city.name} className="featuredImg" />
                 <div className="featuredTitles">
-                  <h1>{cityName}</h1>
-                  <h2>{cityData.count} properties</h2>
+                  <h1>{city.name}</h1>
+                  <h2>{city.hotel_count} hôtels</h2>
                 </div>
               </div>
             );
-          })}
+          })
+      }
     </div>
   );
 }
