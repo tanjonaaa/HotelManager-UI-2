f6d529875d766c92a5741f15573ef4999f426d55
Fiantso Harena <fiantsorav@gmail.com>
2023-08-08 11:12:29 +0300

    feat: Reserve page implement

--- client/src/pages/reserve/Reserve.jsx
+++ client/src/pages/reserve/Reserve.jsx
@@ -1,7 +1,98 @@
-const Reserve = () => {
+import React, { useState } from "react";
+import "./reserve.css"
+
+function Reserve() {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [reservationType, setReservationType] = useState("option1");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Données soumises :", { startDate, endDate, reservationType });
+  };
+
   return (
-    <div>Hello, This is the reserve page</div>
+    <form className="reservation-form" onSubmit={handleSubmit}>
+      <label>
+        Choisissez la plage de dates de séjour:
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          required
+        />
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+          required
+        />
+      </label>
+
+      <br />
+
+      <label>
+        Choisissez une option:
+        <select
+          value={reservationType}
+          onChange={(e) => setReservationType(e.target.value)}
+        >
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      </label>
+
+      <br />
+
+      <button type="submit">Effectuer la Réservation</button>
+    </form>
   );
 }
 
 export default Reserve;
+
+
+/* function ReservationForm() {
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Choisissez la plage de dates de séjour:
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          required
+        />
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+          required
+        />
+      </label>
+
+      <br />
+
+      <label>
+        Choisissez une option:
+        <select
+          value={reservationType}
+          onChange={(e) => setReservationType(e.target.value)}
+        >
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      </label>
+
+      <br />
+
+      <button type="submit">Effectuer la Réservation</button>
+    </form>
+  );
+}
+
+export default ReservationForm; */
